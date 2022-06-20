9648b0f0153b32cadcf6d252cb53fa359b092e20
Jhollyfer Rodrigues <jhollyferr@gmail.com>
2022-06-20 20:32:15 -0300

    :sparkles: home content animations

---
+++ src/js/Animations/index.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.image', { delay: 1000, reset: true });
+ScrollReveal().reveal('.content h1', { delay: 600, reset: true });
+ScrollReveal().reveal('.content p', { delay: 800, reset: true });
+ScrollReveal().reveal('.content button', { delay: 1000, reset: true });
+ScrollReveal().reveal('.content a', { delay: 1000, reset: true });
